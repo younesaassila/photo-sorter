1c83398cb63bee7032e0f3698163df9eb21ae756
Younes <47226184+younesaassila@users.noreply.github.com>
2021-08-05 11:07:23 +0200

    :recycle: Refactor code

---
+++ PhotoSorterLibrary/DirectoryUtility.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PhotoSorterLibrary
+{
+	public class DirectoryUtility
+	{
+		public static List<string> GetFilesWithExtensions(string path, string[] extensions)
+		{
+			return Directory
+				.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+				.Where(file => extensions.Any(file.ToLower().EndsWith))
+				.ToList();
+		}
+
+		public static List<string> GetImages(string path)
+		{
+			return GetFilesWithExtensions(path, new[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tif", ".tiff", ".raw" });
+		}
+
+		public static List<string> GetVideos(string path)
+		{
+			return GetFilesWithExtensions(path, new[] { ".mkv", ".flv", ".avi", ".mov", ".wmv", ".mp4", ".m4v", ".mpeg" });
+		}
+
+		public static string GetDestinationDirectoryPath(string sourceDirectoryPath, string year, string month)
+		{
+			Regex regex = new(@$"{year}-[\d,]*{month}[^\\/‒]*$");
+
+			string path = Directory.GetDirectories(sourceDirectoryPath)
+				.Where(dir => regex.IsMatch(dir))
+				.OrderBy(dir => dir.Length)
+				.ToArray()
+				.FirstOrDefault();
+			if (path == null)
+				path = Directory.CreateDirectory(Path.Join(sourceDirectoryPath, $"{year}-{month}")).FullName;
+			if (!path.EndsWith(Path.DirectorySeparatorChar))
+				path = $"{path}{Path.DirectorySeparatorChar}";
+
+			return path;
+		}
+	}
+}
---
+++ PhotoSorterLibrary/FileSorter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PhotoSorterLibrary
+{
+	public class FileSorter
+	{
+		public static void SortDirectory(string path, bool sortVideos)
+		{
+			Logger.Clear();
+			Logger.WriteLine($"Sorting directory '{path}'... (images {(sortVideos ? "and videos" : "only")})");
+			SortImages(path);
+			if (sortVideos) SortVideos(path);
+			Logger.SaveLog(path);
+		}
+
+		static void SortImages(string path)
+		{
+			List<string> images = DirectoryUtility.GetImages(path);
+			foreach (string image in images)
+			{
+				DateTime? dateTaken = DateTimeUtility.GetDateTakenFromImage(image);
+				if (dateTaken != null)
+				{
+					MoveFile(image, dateTaken.Value);
+				}
+				else
+				{
+					FileInfo fileInfo = new(image);
+					Logger.WriteLine($"Couldn't find date taken for '{fileInfo.Name}'.");
+					return;
+				}
+			}
+		}
+
+		static void SortVideos(string path)
+		{
+			List<string> videos = DirectoryUtility.GetVideos(path);
+			foreach (string video in videos)
+			{
+				FileInfo fileInfo = new(video);
+				DateTime? dateEncoded = DateTimeUtility.GetDateEncodedFromVideo(video);
+				if (dateEncoded != null)
+				{
+					DateTime dateCreated = File.GetCreationTime(video);
+					TimeSpan? timeSpan = dateEncoded - dateCreated;
+					if (timeSpan > new TimeSpan(0, -1, 0) && timeSpan < new TimeSpan(0, 1, 0))
+					{
+						MoveFile(video, dateEncoded.Value);
+					}
+					else
+					{
+						Logger.WriteLine($"Date encoded for '{fileInfo.Name}' is likely incorrect.");
+						return;
+					}
+				}
+				else
+				{
+					Logger.WriteLine($"Couldn't find date encoded for '{fileInfo.Name}'.");
+					return;
+				}
+			}
+		}
+
+		static void MoveFile(string filePath, DateTime date)
+		{
+			FileInfo fileInfo = new(filePath);
+			string year = date.Year.ToString();
+			string month = date.Month.ToString("00");
+
+			string srcDirPath = Path.GetDirectoryName(filePath);
+			string destDirPath = DirectoryUtility.GetDestinationDirectoryPath(srcDirPath, year, month);
+
+			bool fileAlreadyExists =
+				Directory.GetFiles(destDirPath, $"{Path.GetFileNameWithoutExtension(filePath)}.*").Length > 0;
+
+			if (!fileAlreadyExists)
+			{
+				File.Move(filePath, Path.Join(destDirPath, fileInfo.Name));
+			}
+			else
+			{
+				string directoryName = Path.GetFileName(Path.GetDirectoryName(destDirPath));
+				Logger.WriteLine($"'{fileInfo.Name}' already exists in directory '{directoryName}'.");
+				return;
+			}
+		}
+	}
+}
